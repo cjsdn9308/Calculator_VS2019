cf4e391ca2ed8072f864a8227234584f0b148beb
cjsdn9308 <cjsdn9308@naver.com>
2022-06-04 19:58:26 +0900

    초기 커밋.

---
+++ calc/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("굴림", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.MinimumSize = new System.Drawing.Size(280, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 37);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(15, 200);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(63, 53);
+            this.btn1.TabIndex = 1;
+            this.btn1.TabStop = false;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(84, 200);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(63, 53);
+            this.btn2.TabIndex = 2;
+            this.btn2.TabStop = false;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(153, 200);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(63, 53);
+            this.btn3.TabIndex = 3;
+            this.btn3.TabStop = false;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(15, 141);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(63, 53);
+            this.btn4.TabIndex = 4;
+            this.btn4.TabStop = false;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(84, 141);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(63, 53);
+            this.btn5.TabIndex = 5;
+            this.btn5.TabStop = false;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(153, 141);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(63, 53);
+            this.btn6.TabIndex = 6;
+            this.btn6.TabStop = false;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(15, 82);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(63, 53);
+            this.btn7.TabIndex = 7;
+            this.btn7.TabStop = false;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(84, 82);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(63, 53);
+            this.btn8.TabIndex = 8;
+            this.btn8.TabStop = false;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(153, 82);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(63, 53);
+            this.btn9.TabIndex = 9;
+            this.btn9.TabStop = false;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(84, 259);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(63, 53);
+            this.btn0.TabIndex = 10;
+            this.btn0.TabStop = false;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(153, 259);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(63, 53);
+            this.btnResult.TabIndex = 11;
+            this.btnResult.TabStop = false;
+            this.btnResult.Text = "=";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(235, 82);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(63, 53);
+            this.btnSum.TabIndex = 12;
+            this.btnSum.TabStop = false;
+            this.btnSum.Text = "+";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(235, 141);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(63, 53);
+            this.btnSub.TabIndex = 13;
+            this.btnSub.TabStop = false;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(235, 200);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(63, 53);
+            this.btnMul.TabIndex = 14;
+            this.btnMul.TabStop = false;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(235, 259);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(63, 53);
+            this.btnDiv.TabIndex = 15;
+            this.btnDiv.TabStop = false;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(15, 259);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(63, 53);
+            this.btnReset.TabIndex = 16;
+            this.btnReset.TabStop = false;
+            this.btnReset.Text = "C";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(23, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 330);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "계산기";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Key_Down);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ calc/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public partial class Form1 : Form
+    {
+        public static String firstNum = "";         // 첫번째 숫자
+        public static String SecondNum = "";        // 두번째 숫자
+        public static int Result = 0;               // 결과값
+        public static bool logicBool = false;       // 연산기호 누렀는지 확인
+        public static int logicSymbol = 0;          // 연산기호 구별(1:더하기, 2:빼기, 3:곱하기, 4:나누기)
+
+
+
+        bool nonNumEnt = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button10_Click(object sender, EventArgs e)                     // 숫자 0버튼을 눌렀을 때
+        {
+            if (logicBool == false)                             // 연산기호를 누르기 전
+            {
+                firstNum += "0";                // 첫번째 숫자에 0을 입력
+                label1.Text = firstNum;         // 첫번째 숫자를 입력
+            }
+            else                                                // 연산기호를 눌렀을 때
+            {
+                SecondNum += "0";               // 두번째 숫자에 0을 입력
+                label1.Text = SecondNum;        // 두번째 숫자를 입력
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)                     // 숫자 1버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "1";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "1";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)                     // 숫자 2버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "2";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "2";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)                     // 숫자 3버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "3";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "3";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)                     // 숫자 4버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "4";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "4";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)                     // 숫자 5버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "5";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "5";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)                     // 숫자 6버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "6";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "6";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)                     // 숫자 7버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "7";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "7";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)                     // 숫자 8버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "8";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "8";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)                     // 숫자 9버튼을 눌렀을 때
+        {
+            if (logicBool == false)
+            {
+                firstNum += "9";
+                label1.Text = firstNum;
+            }
+            else
+            {
+                SecondNum += "9";
+                label1.Text = SecondNum;
+            }
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)                   // 더하기 버튼을 눌렀을 때
+        {
+            logicSymbol = 1;                                // 연산기호 부여
+            logicBool = true;                               // 연산기호를 눌렀을 다는 것을 구분
+            label2.Text = firstNum + "+";                   // 임시식에 첫번째 숫자와 기호 입력
+            SecondNum = "";                                 // 이전 계산에서 입력된 값을 초기화 
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)                   // 빼기 버튼을 눌렀을 때
+        {
+            logicSymbol = 2;
+            logicBool = true;
+            label2.Text = firstNum + "-";
+            SecondNum = "";
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)                   // 곱하기 버튼을 눌렀을 때
+        {
+            logicSymbol = 3;
+            logicBool = true;
+            label2.Text = firstNum + "*";
+            SecondNum = "";
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)                   // 나누기 버튼을 눌렀을 때
+        {
+            logicSymbol = 4;
+            logicBool = true;
+            label2.Text = firstNum + "/";
+            SecondNum = "";
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)                // 결과버튼을 눌렀을 때
+        {
+            int intfirstNum = Convert.ToInt32(firstNum);                        // String첫번쨰 숫자를 .int첫번째 숫자로 변경
+            int intsecondNum = Convert.ToInt32(SecondNum);                      // String두번쨰 숫자를 .int두번째 숫자로 변경
+
+
+            if (logicSymbol == 1)                                               // 더하기 계산
+            {
+                Result = intfirstNum + intsecondNum;                        // 결과값 계산
+                String stringresult = Convert.ToString(Result);             // 결과값을 String으로 변경
+                label1.Text = stringresult;                                 // 결과값을 출력
+                firstNum = stringresult;                                    // 다음계산을 위해 계산결과값을 첫번째 숫자로 입력
+                label2.Text = intfirstNum + "+" + intsecondNum;             // 임시식에 최종식을 입력
+            }
+            if(logicSymbol == 2)                                               // 빼기 계산
+            {
+                Result = intfirstNum - intsecondNum;
+                String stringresult = Convert.ToString(Result);
+                label1.Text = stringresult;
+                firstNum = stringresult;
+                label2.Text = intfirstNum + "-" + intsecondNum;
+            }
+            if (logicSymbol == 3)                                               // 곱하기 계산
+            {
+                Result = intfirstNum * intsecondNum;
+                String stringresult = Convert.ToString(Result);
+                label1.Text = stringresult;
+                firstNum = stringresult;
+                label2.Text = intfirstNum + "*" + intsecondNum;
+            }
+            if (logicSymbol == 4)                                               // 나누기 계산
+            {
+                Result = intfirstNum / intsecondNum;
+                String stringresult = Convert.ToString(Result);
+                label1.Text = stringresult;
+                firstNum = stringresult;
+                label2.Text = intfirstNum + "/" + intsecondNum;
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)                 // 리셋버튼을 눌렀을 때
+        {
+            firstNum = "";
+            SecondNum = "";
+            Result = 0;
+            logicBool = false;
+            logicSymbol = 0;
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            // 숫자 키다운 이벤트
+
+
+            if (e.KeyCode == Keys.NumPad0|| e.KeyCode == Keys.NumPad1 || e.KeyCode == Keys.NumPad2 || e.KeyCode == Keys.NumPad3 || e.KeyCode == Keys.NumPad4 ||
+                e.KeyCode == Keys.NumPad5 || e.KeyCode == Keys.NumPad6 || e.KeyCode == Keys.NumPad7 || e.KeyCode == Keys.NumPad8 || e.KeyCode == Keys.NumPad9)
+            {
+                if (logicBool == false)
+                {
+                    firstNum += e.KeyData.ToString().Substring(e.KeyData.ToString().Length - 1);
+                    label1.Text = firstNum;
+                }
+                else
+                {
+                    SecondNum += e.KeyData.ToString().Substring(e.KeyData.ToString().Length - 1);
+                    label1.Text = SecondNum;
+                }
+            }
+            
+            // 엔터키 키다운 이벤트
+            else if(e.KeyCode == Keys.Enter)
+            {
+                btnResult_Click(sender, e);
+            }
+
+            // 더하기키 키다운 이벤트
+            else if (e.KeyCode == Keys.Add)
+            {
+                btnSum_Click(sender, e);
+            }
+
+            // 빼기키 키다운 이벤트
+            else if (e.KeyCode == Keys.Subtract)
+            {
+                btnSub_Click(sender, e);
+            }
+
+            // 곱하기 키다운 이벤트
+            else if (e.KeyCode == Keys.Multiply)
+            {
+                btnMul_Click(sender, e);
+            }
+
+            // 나누기키 키다운 이벤트
+            else if (e.KeyCode == Keys.Divide)
+            {
+                btnDiv_Click(sender, e);
+            }
+
+
+
+           
+
+
+        }
+    }
+}
+
